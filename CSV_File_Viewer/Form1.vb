7ab19b956fea66642c23e5fe942da13c94f388fc
Ammaar Eissa <amm_arme2006@hotmail.com>
2021-07-09 16:46:52 +0100

    Initial commit

---
+++ CSV_File_Viewer/Form1.vb
@@ -0,0 +1,53 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Path As String = "C:\Path\To\Your\CSV\Files\data.csv"
+        Me.Text = Path
+        Dim TextFileReader As New FileIO.TextFieldParser(Path)
+
+        TextFileReader.TextFieldType = FileIO.FieldType.Delimited
+        TextFileReader.SetDelimiters(",")
+
+        Dim TextFileTable As DataTable = Nothing
+
+        Dim Column As DataColumn
+        Dim Row As DataRow
+        Dim UpperBound As Int32
+        Dim ColumnCount As Int32
+        Dim CurrentRow As String()
+
+        While Not TextFileReader.EndOfData
+            Try
+                CurrentRow = TextFileReader.ReadFields()
+                If Not CurrentRow Is Nothing Then
+                    ''# Check if DataTable has been created
+                    If TextFileTable Is Nothing Then
+                        TextFileTable = New DataTable("TextFileTable")
+                        ''# Get number of columns
+                        UpperBound = CurrentRow.GetUpperBound(0)
+                        ''# Create new DataTable
+                        For ColumnCount = 0 To UpperBound
+                            Column = New DataColumn()
+                            Column.DataType = System.Type.GetType("System.String")
+                            Column.ColumnName = "Column" & ColumnCount
+                            Column.Caption = "Column" & ColumnCount
+                            Column.ReadOnly = True
+                            Column.Unique = False
+                            TextFileTable.Columns.Add(Column)
+                        Next
+                    End If
+                    Row = TextFileTable.NewRow
+                    For ColumnCount = 0 To UpperBound
+                        Row("Column" & ColumnCount) = CurrentRow(ColumnCount).ToString
+                    Next
+                    TextFileTable.Rows.Add(Row)
+                End If
+            Catch ex As _
+            FileIO.MalformedLineException
+                MsgBox("Line " & ex.Message &
+                "is not valid and will be skipped.")
+            End Try
+        End While
+        TextFileReader.Dispose()
+        DataGridView1.DataSource = TextFileTable
+    End Sub
+End Class
